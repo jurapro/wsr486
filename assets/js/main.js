5fc845eeded6a469fe34e8525577756750991b52
jurapro <jurapro@yandex.ru>
2020-11-02 14:21:47 +0700

    Added class Drawable and Player

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,93 @@
+class Drawble {
+    //Базовые параметры всех объектов
+    constructor(game) {
+        this.game = game;
+        this.$element = this.createElement();
+        this.position = {
+            x: 0,
+            y: 0,
+        };
+        this.size = {
+            w: 0,
+            h: 0,
+        };
+        this.offsets = {
+            x: 0,
+            y: 0,
+        }
+        this.speedPerFrame = 0;
+    }
+
+    //Создание html элемента
+    createElement() {
+        let $element = $(`<div class="element ${this.constructor.name.toLowerCase()}"></div>`);
+        this.game.$zone.append($element);
+        return $element;
+    }
+
+    //Обновление координат элемента
+    update() {
+        this.position.x += this.offsets.x;
+        this.position.y += this.offsets.y;
+    }
+
+    //Применение изменений в css
+    draw() {
+        this.$element.css({
+            left: this.position.x + 'px',
+            top: this.position.y + 'px',
+            width: this.size.w + 'px',
+            height: this.size.h + 'px',
+        })
+    }
+}
+
+class Player extends Drawble {
+    //Начальные параметры игрока
+    constructor(game) {
+        super(game);
+        this.size = {
+            w: 200,
+            h: 50,
+        };
+        this.position = {
+            x: this.game.$zone.width() / 2 - this.size.w / 2,
+            y: this.game.$zone.height() - this.size.h,
+        };
+        this.keys = {
+            ArrowLeft: false,
+            ArrowRight: false,
+        };
+        this.speedPerFrame = 10;
+        this.bindKeyEvents();
+    }
+
+    //Назначение обработчиков нажатия клавиш
+    bindKeyEvents() {
+        document.addEventListener('keydown', ev => this.changeKeyStatus(ev.code, true));
+        document.addEventListener('keyup', ev => this.changeKeyStatus(ev.code, false));
+    }
+
+    //Смена состояния нажатой клавиши
+    changeKeyStatus(code, value) {
+        if (code in this.keys) {
+            this.keys[code] = value;
+        }
+    }
+
+    update() {
+        switch (true) {
+            case this.keys.ArrowLeft:
+                this.position.x -= this.speedPerFrame;
+                break;
+            case this.keys.ArrowRight:
+                this.position.x += this.speedPerFrame;
+                break;
+        }
+    }
+
+}
+
 class Game {
     //Базовые настройки игры
     constructor() {
